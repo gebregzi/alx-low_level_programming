5724f3f4d1889d31c4b07b4f4db4498c2e49e51f
gebregzi <gebregzi7@gmail.com>
2022-04-07 06:27:26 -0700

    llp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,26 @@
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: Girma G
+ *
+ *    */
+
+
 #include <stdio.h>
+/**
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *   *                puzzle, followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+
 int main(void)
 
 {
