e00e163aa9e0560d222d8242f2dcc60d2d8c0a3a
gebregzi <gebregzi7@gmail.com>
2022-04-11 06:16:48 -0700

    variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+
+{
+
+		char c, d;
+		for (c = 'a'; c <= 'z'; c++)
+				putchar(c);
+		for (d = 'A'; d <= 'Z'; d++)
+				putchar(d);
+		putchar('\n');
+		return (0);
+}
