603dc23e2ebfa0389a7f07712895070c2ba15ee3
gebregzi <gebregzi7@gmail.com>
2022-04-12 06:03:46 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _putchar write character c to std out
+ *
+ * Return: 0.
+ */
+int main(void)
+
+{
+char c[8] = "_putchar";
+int i = 0;
+
+while (i < 8){
+_putchar(c[i]);										i++;								
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *
+ *  * File: holberton.h
+ *
+ *   * Auth: gitma g
+ *
+ *    * Desc: Header file containing declarations for all functions
+ *
+ *     *       used in the 0x02-functions_nested_loops directory.
+ *
+ *      */
+int _putchar(char c);
+void print_alphabet(void);
+#endif
+
