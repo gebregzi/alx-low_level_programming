4cc206a0bfd1c466aa1a188e4c051e53a4e70087
gebregzi <gebregzi7@gmail.com>
2022-04-12 06:43:48 -0700

    functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * print_sign -print sign of numbers
+ * return -1 if it is negative
+ * Return: 1 if it is postive
+ * 	0 if it is zero
+ *	-1 if it is negative
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return(0);
+}
+else
+{
+_putchar('-');
+return(-1);
+}
+}
