7203c70796e4e875f8d42242a2faca02053d5737
gebregzi <gebregzi7@gmail.com>
2022-04-13 06:39:06 -0700

    functions

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
-#include"main.h"
+#include "main.h"
 /*
  * add - to sum numbers
+ *@a: first number input
+ *@b: second number input
  * Return: sum
  *
  */
 int add(int a, int b)
 {
- return a+b;
+ return (a+b);
 }
