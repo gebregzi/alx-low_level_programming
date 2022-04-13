04276da92ca4a02be33d74ef164220db4a1850f1
gebregzi <gebregzi7@gmail.com>
2022-04-13 05:40:00 -0700

    debug

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main- causes infinite loop
+ * Return :0
+ */
+int main(void)
+{
+int i;
+while(i<10)
+{
+putchar(i);
+}
+printf("infinite loop avoided \\o/\n");
+return 0;
+}
