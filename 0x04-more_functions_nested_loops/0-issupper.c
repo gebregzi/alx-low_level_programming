3c72388becee9e5a94214952c4bffec99b3fb870
gebregzi <gebregzi7@gmail.com>
2022-04-14 06:00:49 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/0-issupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper -checks uppers and lower case
+ * Return:1 if is upper else return 0.
+*/
+ int _isupper(int c)
+{
+if(c>='A' && c<='Z')
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
+
+
+
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(int c);
+
+int _isupper(int c);
+
+#endif
