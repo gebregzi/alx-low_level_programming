0dbd56d5ce7c4e90c2288808ac3c9c9410eea1c6
gebregzi <gebregzi7@gmail.com>
2022-04-20 00:52:33 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,55 @@
+#include"main.h"
+
+/**
+ *
+ *  * _strcat - concatenates two strings together
+ *
+ *   * @dest: variable string to be appended
+ *
+ *    * @src: variable string to be appended
+ *
+ *     *
+ *
+ *      * Return: pointer to the resulting string dest
+ *
+ *       */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+		int i;
+
+			int j;
+
+
+
+				i = 0;
+
+					while (dest[i] != '\0')
+
+							{
+
+										i++;
+
+											}
+
+						j = 0;
+
+							while (src[j] != '\0')
+
+									{
+
+												dest[i] = src[j];
+
+														i++;
+
+																j++;
+
+																	}
+
+								dest[i] = '\0';
+
+									return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H_
+
+#define MAIN_H_
+
+
+
+
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+
+
+
+
+#endif
